c4e5ebf2578bc317d1627de96f027f6dc2a8691a
alumno <alumno@26-04>
2015-05-21 17:09:25 +0200

    Añadimos las instancias y la crecaion de la venta

--- Tienda_Victor_004/src/tienda/Principal.java
+++ Tienda_Victor_004/src/tienda/Principal.java
@@ -4,7 +4,22 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
-	
+		
+		Clientes cliente1 = new Clientes();
+		Clientes cliente2 = new Clientes();
+		Clientes cliente3 = new Clientes();
+		
+		Productos pro1 = new Productos();
+		Productos pro2 = new Productos();
+		Productos pro3 = new Productos();
+		
+		Vendedores vend1 = new Vendedores();
+		Vendedores vend2 = new Vendedores();
+		Vendedores vend3 = new Vendedores();
+		
+		Tiendas tien1 = new Tiendas();
+		Tiendas tien2 = new Tiendas();
+		Tiendas tien3 = new Tiendas();
 	}
 
 }
--- Tienda_Victor_004/src/tienda/Venta.java
+++ Tienda_Victor_004/src/tienda/Venta.java
@@ -1,35 +1,16 @@
 package tienda;
 
+import java.util.Date;
+
 
 public class Venta {
 	
-	private String id_producto;
-	private String id_cliente;
-	private String id_vendedor;
-	private String id_tienda;
-	String getId_tienda() {
-		return id_tienda;
-	}
-	void setId_tienda(String id_tienda) {
-		this.id_tienda = id_tienda;
-	}
-	String getId_vendedor() {
-		return id_vendedor;
-	}
-	void setId_vendedor(String id_vendedor) {
-		this.id_vendedor = id_vendedor;
-	}
-	String getId_cliente() {
-		return id_cliente;
-	}
-	void setId_cliente(String id_cliente) {
-		this.id_cliente = id_cliente;
-	}
-	String getId_producto() {
-		return id_producto;
-	}
-	void setId_producto(String id_producto) {
-		this.id_producto = id_producto;
-	}
-
+		Date ahora = new Date();
+		
+		public void nuevaVenta(Clientes cliente1, Productos pro1, Tiendas tien1,
+				Vendedores vend1, Date ahora){
+			String nuevaVenta;
+			nuevaVenta= cliente1+" "+pro1+" "+tien1+" "+vend1+" "+ahora;
+			System.out.println(nuevaVenta);
+		}
 }
